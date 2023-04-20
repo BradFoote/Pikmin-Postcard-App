38479127f7f1619ee1777e71abef3fab20426493
Brad Foote <brad.foote95@yahoo.com>
2023-04-20 13:55:53 +0900

    4/20 resuming work

--- models/Postcard.js
+++ models/Postcard.js
@@ -29,3 +29,7 @@ const PostcardSchema = new mongoose.Schema({
 
 //MongoDB Collection named here - will give lowercase plural of name 
 module.exports = mongoose.model("Postcard", PostcardSchema);
+
+
+//want to create a feed from the postcard section that viewers can like and share or save to their portfolio. Also add some sort fo favorite
+//section that can be displayed on each profile.
